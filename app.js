45e58d659f4aba196f333b668a680614d894c9bf
Winston <caswinst@amazon.com>
2024-03-27 12:13:39 -0700

    second commit

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ document.addEventListener('DOMContentLoaded', function() {
     const header = document.getElementById('header');
     
     header.addEventListener('mousemove', function(e) {
-        const moveX = (e.clientX / window.innerWidth * 10) - 5; // Adjust the multiplier to control the movement intensity
-        const moveY = (e.clientY / window.innerHeight * 10) - 5;
+        const moveX = (e.clientX / window.innerWidth * 15) - 5; // Adjust the multiplier to control the movement intensity
+        const moveY = (e.clientY / window.innerHeight * 15) - 5;
 
         header.style.backgroundPositionX = moveX + 'px';
         header.style.backgroundPositionY = moveY + 'px';
